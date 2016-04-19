96e8a70880e7b8362b45308f99e72df4c57493c1
taapesh <starmansix@gmail.com>
2016-04-19 15:45:49 -0500

    Add whitenoise for static files

--- r2d2/wsgi.py
+++ r2d2/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for r2d2 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "r2d2.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
